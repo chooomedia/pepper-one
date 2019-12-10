2bfcbea1bc638e0960d2c8155f2127903aef7f62
Christopher Matt <christopher@od.cuciniale.com>
2019-12-10 15:56:42 +0100

    styled partner-page

--- template-parts/content-partner.php
+++ template-parts/content-partner.php
@@ -8,13 +8,12 @@
  */
 
 ?>
-<?php $recipeLink = get_field( 'recipe-button-link' ); ?>
-<?php $recipeLinkTitle = get_field( 'recipe-button-link-title' ); ?>
+
 <div class="container-fluid p-0">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="post-thumbnail" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');max-height:40vh;background-position:center;"></div>
 		<div class="container">
-			<header class="entry-header my-5">
+			<header class="entry-header mt-5">
 				<?php
 				if ( is_single() ) :
 					the_title( '<h2 class="text-center">', '</h2>' );
@@ -41,8 +40,9 @@
 					?>
 				</div>
 			</div><!-- .entry-content -->
-
-			<footer class="entry-footer">
+			<!-- $partnerLink = get_field( 'partner-store-link' ); -->
+			<footer class="entry-footer mb-4">
+				<a href="#partner" class="col-md-4 btn btn-cta btn-dark btn-appointment"><i class="fas fa-calendar"></i> Make an appointment</a>
 				<?php wp_template_pepper_one_entry_footer(); ?>
 			</footer><!-- .entry-footer -->
 		</div>
