b85e704495e5c46a85a694b8c42d788c817fcb5b
Christopher Matt <christopher@od.cuciniale.com>
2019-12-11 18:06:22 +0100

    sidebar nav changes

--- inc/assets/js/sidebar-nav.js
+++ inc/assets/js/sidebar-nav.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function () {
         jQuery("#sidebar").removeClass("active");
         // hide overlay
         jQuery(".overlay").removeClass("active");
+        jQuery(".overlay").css("z-index", "-1");
     });
 
     jQuery("#sidebarCollapse").on("click", function () {
@@ -15,6 +16,7 @@ jQuery(document).ready(function () {
         // fade in the overlay
         jQuery(".overlay").addClass("active");
         jQuery(".overlay").css("top", headerHeight);
+        jQuery(".overlay").css("z-index", "998");
         jQuery(".collapse.in").toggleClass("in");
         jQuery("a[aria-expanded=true]").attr("aria-expanded", "false");
     });
