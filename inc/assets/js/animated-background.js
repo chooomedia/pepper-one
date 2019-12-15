d7352a5d0aa385762e14d6fc5f45a80e598e8cf6
Chris <christopher@chooo.media>
2019-12-16 00:18:37 +0100

    animated-background on scroll final2 :)

--- inc/assets/js/animated-background.js
+++ inc/assets/js/animated-background.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
     $(document).scroll(function(){
         var x = $(this).scrollTop();
-        $('div.post-thumbnail').css('transition', 'all .5s');
+        $('div.post-thumbnail').css('transition', 'all 2s');
         $('div.post-thumbnail').css('background-position', '100%' + parseInt(-x / 3.75) + 'px' + ', 0%' + parseInt(-x / 4.66) + 'px, center top');
         $('div.post-thumbnail').css('background-position-x', '-'+ parseInt(-x / 3.75) + '%');
     });
