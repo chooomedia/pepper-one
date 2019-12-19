9a45ab6493e2bccc215f2cca3fc7c4607566f279
chooomedia <christopher@chooomedia.de>
2019-12-19 15:04:23 +0100

    404.php text changed into german lang. & style definitions for wp-forms input-elements

--- 404.php
+++ 404.php
@@ -14,11 +14,11 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header mt-5 pt-3">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wp-pepper-one' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Oops! Hier gibt es leider nichts.', 'wp-pepper-one' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content mb-5 pb-3">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp-pepper-one' ); ?></p>
+					<p><?php esc_html_e( 'Es sieht so aus, als wäre an diesem Ort nichts gefunden worden.', 'wp-pepper-one' ); ?></p>
 
 					<?php
 						get_search_form();
