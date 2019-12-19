769ba1b23bffa311a41b6a8da90e97d379469da9
chooomedia <christopher@chooomedia.de>
2019-12-19 18:27:48 +0100

    added justify-middle on footer-widget left and right area & added image resize for footer-widget

--- footer-widget.php
+++ footer-widget.php
@@ -5,13 +5,13 @@ if ( is_active_sidebar( 'footer-left' ) || is_active_sidebar( 'footer-middle' ) 
             <div class="d-flex justify-content-center">
                 <div class="row">
                     <?php if ( is_active_sidebar( 'footer-left' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-left' ); ?></div>
+                        <div class="col-12 col-md-4 d-flex align-items-center"><?php dynamic_sidebar( 'footer-left' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-middle' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-middle' ); ?></div>
+                        <div class="col-12 col-md-4 d-flex align-items-center"><?php dynamic_sidebar( 'footer-middle' ); ?></div>
                     <?php endif; ?>
                     <?php if ( is_active_sidebar( 'footer-right' )) : ?>
-                        <div class="col-12 col-md-4"><?php dynamic_sidebar( 'footer-right' ); ?></div>
+                        <div class="col-12 col-md-4 d-flex align-items-center"><?php dynamic_sidebar( 'footer-right' ); ?></div>
                     <?php endif; ?>
                 </div>
             </div>
