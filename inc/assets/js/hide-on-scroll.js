fbd48e30ce830b927edd6e41729f3bc614de6c8b
chooomedia <christopher@chooomedia.de>
2020-01-07 10:01:12 +0100

    added show/hide on social-media-buttons #happynewyearpush

--- inc/assets/js/hide-on-scroll.js
+++ inc/assets/js/hide-on-scroll.js
@@ -2,14 +2,20 @@ jQuery( function ( $ ) {
     $(window).scroll(function() {    
         var circledButtons = $(".circled-buttons");
         var scroll = $(window).scrollTop();
-    
+
         if (scroll >= 200) {
             circledButtons.addClass("d-none-scroll");
             $("#sidebar").removeClass("active");
             $(".overlay").removeClass("active");
             $(".overlay").css("z-index", "-1");
+
         } else {
             circledButtons.removeClass("d-none-scroll");
+        }
+
+        if (scroll + $(window).height() == $(document).height()) {
+            circledButtons.removeClass("d-none-scroll");
+            $(".btn-social:last-of-type").show();
         }
     
         $("#colophon").on("mouseover", function() {
