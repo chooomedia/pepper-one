04a7232dee8814e666054debd67189f0996d97c3
chooomedia <christopher@chooomedia.de>
2020-01-10 16:41:13 +0100

    custom field var recipe short description added

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,7 @@
 
 <?php $recipeLink = get_field( 'recipe-button-link' ); ?>
 <?php $recipeLinkTitle = get_field( 'recipe-button-link-title' ); ?>
+<?php $recipeShortDescr = get_field( 'recipe-short-description' ); ?>
 <div class="container-fluid p-0">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<div class="post-thumbnail" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');">
@@ -27,7 +28,7 @@
 				<div class="col-md-6 offset-md-6 d-flex flex-row-reverse bd-highlight h-100 p-4">
 					<div class="m-4 bd-highlight justify-content-center align-self-center post-thumbnail-inner-content">
 						<h1>Partners ut ex <br>vitae enim</h1>
-						<p class="recipes">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.</p>
+						<p class="recipes"><?php echo $recipeShortDescr ?></p>
 						<a href="<?php echo $recipeLink ?>" target="_blank" title="<?php echo $recipeLinkTitle ?>" class="btn btn-primary btn-bigger">Cook it with the App</a>
 					</div>
 				</div>
