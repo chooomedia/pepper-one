a69f581adeb04f55cb94e725b6948fbc247ab969
chooomedia <christopher@chooomedia.de>
2020-01-14 16:43:29 +0100

    removed unnescary remove-title function changed spaces-class on footer

--- functions.php
+++ functions.php
@@ -77,17 +77,6 @@ endif;
 add_action( 'after_setup_theme', 'wp_template_pepper_one_setup' );
 
 
-/* Function that hide Titles in Pages and Posts*/
-function remove_single_custom_post_titles( $title ) {
-    if( is_page() ):
-        return '';
-    else:
-        return $title;
-    endif;
-}
-add_filter( 'the_title', 'remove_single_custom_post_titles', 10, 2 );
-
-
 /* Add Custom Post Types for easier Content management */
 // Register Custom Post Type
 function wp_template_pepper_cpt_recipes() {
