9430567241d9a14caec332f41a3b713013171ba3
chooomedia <christopher@chooomedia.de>
2020-02-11 16:02:56 +0100

    change grid inside recipes loop

--- archive-recipes.php
+++ archive-recipes.php
@@ -30,10 +30,10 @@ get_header(); ?>
 
 						if ( $date_variable != $year ) // Year values are not the same
 							echo '	<div class="row mx-1 col-12">
-										<div class="col-md-4 float-left">
+										<div class="col-md-4 col-5 float-left">
 											<h2>' . $year . '</h2>
 										</div>
-										<div class="col-md-8 text-right">
+										<div class="col-md-8 col-7 text-right">
 											<p class="recipe-app-info mr-md-4">
 												In der Groumetpilot2 App <br>digital verfügbar
 											</p>
